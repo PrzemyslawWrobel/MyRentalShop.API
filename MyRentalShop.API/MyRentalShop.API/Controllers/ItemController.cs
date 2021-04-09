eb1dbde5be52c6522da026e9506aace2a0d939fe
PW <przemek.wrobel@poczta.fm>
2021-04-09 12:30:52 +0100

    Dodany ItemController

---
+++ MyRentalShop.API/MyRentalShop.API/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Internal;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.API.Controllers
+{
+    /// <summary>
+    /// Kontroler odp za obsługę produktów
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        /// <summary>
+        /// Zwraca listę produktów
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
+        public async Task<ActionResult<string>> GetItemsAsync()
+        {
+            return "Item Controller";
+        }
+    }
+}
