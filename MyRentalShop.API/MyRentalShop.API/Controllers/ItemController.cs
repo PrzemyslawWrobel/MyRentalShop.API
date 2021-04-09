daeb396e8a2d486c6dd8c42f3f1ba86abbc1210b
PW <przemek.wrobel@poczta.fm>
2021-04-09 14:47:27 +0100

    using do EnableCors

--- MyRentalShop.API/MyRentalShop.API/Controllers/ItemController.cs
+++ MyRentalShop.API/MyRentalShop.API/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.UI.V4.Pages.Internal;
 using Microsoft.AspNetCore.Mvc;
 using System;
