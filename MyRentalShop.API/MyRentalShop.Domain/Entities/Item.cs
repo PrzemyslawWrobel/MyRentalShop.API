814065ee25e40578fd984fddf7528ed5606e791a
PW <przemek.wrobel@poczta.fm>
2021-04-15 13:53:11 +0100

    Update Item

--- MyRentalShop.API/MyRentalShop.Domain/Entities/Item.cs
+++ MyRentalShop.API/MyRentalShop.Domain/Entities/Item.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace MyRentalShop.Domain.Entities
 {
-    class Item
+    public class Item
     {
+        /// <summary>
+        /// Id produktu autoinkrementacja na bazie
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nazwa produktu
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// rozszerzona nazwa produktu
+        /// </summary>
+        public string SecondName { get; set; }
+
+        /// <summary>
+        /// Nr inwentarzowy np kod kreskowy, lub nr rejestracyjny pojazdu
+        /// </summary>
+        public int InventoryNumber { get; set; }
+
+        /// <summary>
+        /// jakiego typu jest produkt
+        /// </summary>
+        public int TypeId { get; set; }
+
+        /// <summary>
+        /// Minimalny wiek wypożyczającego, sprzęt narzędzia
+        /// </summary>
+        public int MinAgeBorrower { get; set; }
+
     }
 }
