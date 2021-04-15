6b4b1d3d204944f2d9b847e0e411bb5b3cc48b16
PW <przemek.wrobel@poczta.fm>
2021-04-15 13:24:24 +0100

    Utworzona klasa Customer

--- MyRentalShop.API/MyRentalShop.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MyRentalShop.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ MyRentalShop.API/MyRentalShop.Domain/Entities/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nazwa klienta indywidualnego lub firmy
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Data rejestracji użytkownika
+        /// </summary>
+        public DateTime RegistrationDate { get; set; }
+
+        /// <summary>
+        /// Wiek klienta indywidualnego, wymagany do wyporzyczenia niektórych aut
+        /// </summary>
+        public int AgeCustomer { get; set; }
+
+        /// <summary>
+        /// Status informujący czy użytkownik może wypożyczać produkty
+        /// </summary>
+        public int CustomerStatusId { get; set; }
+
+        /// <summary>
+        /// NIP firmy wypożyczającej
+        /// </summary>
+        public string NIP { get; set; }
+
+        /// <summary>
+        /// Regon firmy wypożyczającej
+        /// </summary>
+        public string REGON { get; set; }
+
+        /// <summary>
+        /// Czy klient jest aktywny
+        /// </summary>
+        public bool IsActiv { get; set; }
+
+    }
+}
