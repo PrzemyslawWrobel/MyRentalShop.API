6b8f6249b67d2482cf05da57adc1999f7dcc3b60
PW <przemek.wrobel@poczta.fm>
2021-04-16 13:17:16 +0100

    Dodana klasa Adress

---
+++ MyRentalShop.API/MyRentalShop.Domain/Entities/Address.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Domain.ValueObjects
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Jaki adres domowy, do korespondencji, zamieszkanaia, wykonywania działalności
+        /// </summary>
+        public string AddressType { get; set; }
+
+        /// <summary>
+        /// Nazwa ulicy
+        /// </summary>
+        public string Street { get; set; }
+
+        /// <summary>
+        /// Numer Bloku, Domu
+        /// </summary>
+        public string BuildingNumber { get; set; }
+
+        /// <summary>
+        /// Numer mieszkania
+        /// </summary>
+        public int FlatNumber { get; set; }
+
+        /// <summary>
+        /// Kod pocztowy
+        /// </summary>
+        public string ZipCode { get; set; }
+
+        /// <summary>
+        /// Nazwa miasta, Wsi
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Nazwa Kraju
+        /// </summary>
+        public string Country { get; set; }
+    }
+}
