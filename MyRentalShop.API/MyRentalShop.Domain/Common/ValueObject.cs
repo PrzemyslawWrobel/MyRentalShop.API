b60018e8c58251cb4856eb5b53f2a01c59642f13
PW <przemek.wrobel@poczta.fm>
2021-04-16 11:54:06 +0100

    Dodana Klasa ValueObject

--- MyRentalShop.API/MyRentalShop.Domain/Common/ValueObject.cs
+++ MyRentalShop.API/MyRentalShop.Domain/Common/ValueObject.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyRentalShop.Domain.ValueObjects
+namespace MyRentalShop.Domain.Common
 {
-    public class Address
+    class ValueObject
     {
     }
 }
--- MyRentalShop.API/MyRentalShop.Domain/ValueObjects/PersonName.cs
+++ MyRentalShop.API/MyRentalShop.Domain/ValueObjects/PersonName.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyRentalShop.Domain.ValuesObject
+namespace MyRentalShop.Domain.ValueObjects
 {
     public class PersonName
     {
