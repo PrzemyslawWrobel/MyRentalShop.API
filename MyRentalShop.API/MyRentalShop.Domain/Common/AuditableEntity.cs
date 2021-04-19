115b8c8f644a69d20eda211d0f2e778b4e57a174
PW <przemek.wrobel@poczta.fm>
2021-04-19 11:13:02 +0100

    Utworzenie clasy Auditable

---
+++ MyRentalShop.API/MyRentalShop.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DataType Created { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? Modified { get; set; }
+    }
+}
