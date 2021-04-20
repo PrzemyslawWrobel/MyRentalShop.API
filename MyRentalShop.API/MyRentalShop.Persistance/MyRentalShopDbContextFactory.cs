83de499db35e9ad5bdc0b3f311f3d637a821be8c
PW <przemek.wrobel@poczta.fm>
2021-04-20 15:29:58 +0100

    Db context cd ..

---
+++ MyRentalShop.API/MyRentalShop.Persistance/MyRentalShopDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance
+{
+    public class MyRentalShopDbContextFactory : DesignTimeDbContextFactoryBase<MyRentalShopDbContext>
+    {
+        protected override MyRentalShopDbContext CreateNewInstance(DbContextOptions<MyRentalShopDbContext> options)
+        {
+            return new MyRentalShopDbContext(options);
+        }
+    }
+}
