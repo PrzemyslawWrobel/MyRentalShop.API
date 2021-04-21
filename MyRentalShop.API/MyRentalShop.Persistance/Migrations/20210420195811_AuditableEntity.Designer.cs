de6478cae0edb7f6b64398fa34056c0254bcd0e1
PW <przemek.wrobel@poczta.fm>
2021-04-21 09:43:33 +0100

    Migracjia - uzupełnienie bazy o AuditableEntity

--- MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210420195811_AuditableEntity.Designer.cs
+++ MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210420195811_AuditableEntity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyRentalShop.Persistance;
 
 namespace MyRentalShop.Persistance.Migrations
 {
     [DbContext(typeof(MyRentalShopDbContext))]
-    partial class MyRentalShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210420195811_AuditableEntity")]
+    partial class AuditableEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210420195811_AuditableEntity.cs
@@ -0,0 +1,518 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRentalShop.Persistance.Migrations
+{
+    public partial class AuditableEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ItemTypes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ItemTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "ItemTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "ItemTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ItemTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ItemTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "ItemTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ItemTags",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ItemTags",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "ItemTags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "ItemTags",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ItemTags",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ItemTags",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "ItemTags",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Items",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Items",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Items",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "CustomerTypes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "CustomerTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "CustomerTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "CustomerTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "CustomerTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "CustomerTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "CustomerTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Customers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "ContactPersons",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "ContactPersons",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "ContactPersons",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "ContactPersons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Addresses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inactivated",
+                table: "Addresses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InactivatedBy",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Modified",
+                table: "Addresses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ItemTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ItemTags");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "CustomerTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ContactPersons");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Inactivated",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "InactivatedBy",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Modified",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Addresses");
+        }
+    }
+}
