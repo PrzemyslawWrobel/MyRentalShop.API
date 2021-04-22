a234abf5d4668d517f04bbba8bf7bdf77c00d308
PW <przemek.wrobel@poczta.fm>
2021-04-22 14:51:06 +0100

    Konfiguracja dla bazy danych Customer

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRentalShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(300).IsRequired();
+        }
+    }
+}
