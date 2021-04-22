3bcd02f2dab2fb9a242ca16368e6b4e485ea0705
PW <przemek.wrobel@poczta.fm>
2021-04-22 15:54:43 +0100

    Dodana Konfiguracja dla Adresu

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Configurations/AddressConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRentalShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance.Configurations
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.Property(p => p.AddressType).HasMaxLength(100);
+            builder.Property(p => p.Street).HasMaxLength(100);
+            builder.Property(p => p.BuildingNumber).HasMaxLength(100);
+            builder.Property(p => p.FlatNumber).HasMaxLength(100);
+            builder.Property(p => p.ZipCode).HasMaxLength(100);
+            builder.Property(p => p.City).HasMaxLength(100);
+            builder.Property(p => p.Country).HasMaxLength(100);
+        }
+    }
+}
