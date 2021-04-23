2ec3d89b6ae10df58770a44870b34f1404124558
PW <przemek.wrobel@poczta.fm>
2021-04-23 13:44:58 +0100

    Obsługa systemu plików

---
+++ MyRentalShop.API/MyRentalShop.Infrastructure/FileStore/DirectoryWrapper.cs
@@ -0,0 +1,18 @@
+﻿using MyRentalShop.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Infrastructure.FileStore
+{
+    public class DirectoryWrapper : IDirectoryWrapper
+    {
+        public void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
---
+++ MyRentalShop.API/MyRentalShop.Infrastructure/FileStore/FileWrapper.cs
@@ -0,0 +1,18 @@
+﻿using MyRentalShop.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Infrastructure.FileStore
+{
+    public class FileWrapper : IFileWrapper
+    {
+        public void WriteAllBytes(string outputFile, byte[] content)
+        {
+            File.WriteAllBytes(outputFile, content);
+        }
+    }
+}
