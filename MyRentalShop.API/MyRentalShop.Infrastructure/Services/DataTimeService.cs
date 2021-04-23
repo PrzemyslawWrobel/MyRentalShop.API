3ea1e200cb0cb2481691a22fe706f47830faefe8
PW <przemek.wrobel@poczta.fm>
2021-04-23 11:48:04 +0100

    Implementacja IDataTime

--- MyRentalShop.API/MyRentalShop.Infrastructure/Services/DataTimeService.cs
+++ MyRentalShop.API/MyRentalShop.Infrastructure/Services/DataTimeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyRentalShop.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace MyRentalShop.Infrastructure.Services
 {
     public class DataTimeService : IDataTime
     {
-
+        public DateTime Now => DateTime.Now;
     }
 }
