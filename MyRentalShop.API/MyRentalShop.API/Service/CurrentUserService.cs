fada1ae8a35e37b5c05c2bba7f1b1ff8cab63caa
PW <przemek.wrobel@poczta.fm>
2021-07-16 11:53:43 +0100

    Dodana klasa Current user service

---
+++ MyRentalShop.API/MyRentalShop.API/Service/CurrentUserService.cs
@@ -0,0 +1,25 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.API.Service
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public CurrentUserService(IHttpContextAccessor httpContextAcessor)
+        {
+            var email = httpContextAcessor.HttpContext?.User?.FindFirstValue(JwtClaimTypes.Email);
+
+            Email = email;
+
+            IsAuthenticated = !string.IsNullOrEmpty(email);
+
+        }
+    }
+}
