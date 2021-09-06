f47832d7d898bcd9a998434f85d351a7c2d02184
PW <przemek.wrobel@poczta.fm>
2021-09-06 15:15:17 +0200

    email exceptions

--- MyRentalShop.API/MyRentalShop.Domain/ValueObjects/Email.cs
+++ MyRentalShop.API/MyRentalShop.Domain/ValueObjects/Email.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyRentalShop.Domain.Common;
+using MyRentalShop.Domain.CustomExceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace MyRentalShop.Domain.ValueObjects
 {
-    class Email
+    public class Email : ValueObject
     {
+        public string UserName { get; set; }
+        public string DomainName { get; set; }
+
+        public static Email For(string email)
+        {
+            var emailObj = new Email();
+            try
+            {
+                var index = email.IndexOf("@", StringComparison.Ordinal);
+                emailObj.UserName = email.Substring(0, index);
+                emailObj.DomainName = email.Substring(index + 1);
+            }
+            catch (Exception ex)
+            {
+
+                throw new EmailException(email, ex);
+            }
+
+            return emailObj;
+
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return UserName;
+            yield return DomainName;
+        }
     }
 }
