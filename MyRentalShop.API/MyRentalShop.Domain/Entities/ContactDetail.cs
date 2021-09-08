ef5c6070182d4433be4d1a7efab2ff306ded8db1
PW <przemek.wrobel@poczta.fm>
2021-09-08 13:52:11 +0200

    Dodana migracjia ContactDetails, Dodane Dbsety

--- MyRentalShop.API/MyRentalShop.Domain/Entities/ContactDetail.cs
+++ MyRentalShop.API/MyRentalShop.Domain/Entities/ContactDetail.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace MyRentalShop.Domain.Entities
 {
-    class ContactDetail
+    public class ContactDetail
     {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Przechowuje telefony, emil
+        /// </summary>
+        public string ContactDetailInformation { get; set; }
+
+        /// <summary>
+        /// klucz obcy 
+        /// </summary>
+        public int ContactDetailTypeId { get; set; }
+
+        /// <summary>
+        /// przechowuje w obiekcie informacje czy to jest telefon czy adres email
+        /// </summary>
+        public ContactDetailType ContactDetailType { get; set; }
+
+        ///właściwości wskazujące na tabele klienta 
+        /// <summary>
+        /// Klucz obcy wskazujący na tabelę Customer - kolumnę z ID
+        /// </summary>
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
     }
 }
---
+++ MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210908114758_ContactDetails.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRentalShop.Persistance.Migrations
+{
+    public partial class ContactDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PersonName_SecondName",
+                table: "ContactPersons",
+                newName: "SecondName");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonName_LastName",
+                table: "ContactPersons",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonName_FirstName",
+                table: "ContactPersons",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "ContactPersons",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "ContactPersons",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "ContactPersons",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlatNumber",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BuildingNumber",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressType",
+                table: "Addresses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 8, 13, 47, 57, 991, DateTimeKind.Local).AddTicks(1447));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 8, 13, 47, 57, 995, DateTimeKind.Local).AddTicks(8902));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SecondName",
+                table: "ContactPersons",
+                newName: "PersonName_SecondName");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "ContactPersons",
+                newName: "PersonName_LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "ContactPersons",
+                newName: "PersonName_FirstName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonName_SecondName",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonName_LastName",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonName_FirstName",
+                table: "ContactPersons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlatNumber",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BuildingNumber",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressType",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 4, 22, 12, 42, 23, 964, DateTimeKind.Local).AddTicks(9150));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 4, 22, 12, 42, 23, 966, DateTimeKind.Local).AddTicks(6864));
+        }
+    }
+}
