f2ebe0722f8ba59b4e7cf4714a0750f798c13d2d
PW <przemek.wrobel@poczta.fm>
2021-09-08 10:17:37 +0200

    Dodany kontroler do obsługi reserwacji

---
+++ MyRentalShop.API/MyRentalShop.API/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.API.Controllers
+{
+    public class ReservationController : Controller
+    {
+      
+        /// <summary>
+        /// Zwraca listę reserwacji
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
+        public async Task<ActionResult<string>> GetReservationsAsync()
+        {
+            return "Customer Controller";
+        }
+    }
+}
