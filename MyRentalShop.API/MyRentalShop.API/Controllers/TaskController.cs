bc82ace80d1e48683ecc0208cf717cc9dacfaab7
PW <przemek.wrobel@poczta.fm>
2021-09-08 10:21:27 +0200

    Dodany kontroler do obsługi zadań, modyfikacja kontrolera rezerwacji

--- MyRentalShop.API/MyRentalShop.API/Controllers/TaskController.cs
+++ MyRentalShop.API/MyRentalShop.API/Controllers/TaskController.cs
@@ -9,25 +9,24 @@ using System.Threading.Tasks;
 namespace MyRentalShop.API.Controllers
 {
     /// <summary>
-    /// Kontroler odp za obsługę Rezerwacji
+    /// Kontroler odp za obsługę zadań
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     [EnableCors("MyAllowAngularOrigins")]
-    public class ReservationController : Controller
+    public class TaskController : Controller
     {
-      
         /// <summary>
-        /// Zwraca listę reserwacji
+        /// Zwraca listę zadań
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         //[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
-        public async Task<ActionResult<string>> GetReservationsAsync()
+        public async Task<ActionResult<string>> GetTasksAsync()
         {
-            return "Reservation Controller";
+            return "Task Controller";
         }
     }
 }
