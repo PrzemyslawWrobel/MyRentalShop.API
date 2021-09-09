7b52bb466e6ca67964f1b7e11082d72e3a1394f8
PW <przemek.wrobel@poczta.fm>
2021-09-09 13:59:37 +0200

    Zrobiona Migracja

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210909115243_ContactPersonRequired.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRentalShop.Persistance.Migrations
+{
+    public partial class ContactPersonRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactDetailTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactDetailTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactDetailInformation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactDetailTypeId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactDetails_ContactDetailTypes_ContactDetailTypeId",
+                        column: x => x.ContactDetailTypeId,
+                        principalTable: "ContactDetailTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContactDetails_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 13, 52, 42, 412, DateTimeKind.Local).AddTicks(5654));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 13, 52, 42, 417, DateTimeKind.Local).AddTicks(759));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactDetails_ContactDetailTypeId",
+                table: "ContactDetails",
+                column: "ContactDetailTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactDetails_CustomerId",
+                table: "ContactDetails",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactDetails");
+
+            migrationBuilder.DropTable(
+                name: "ContactDetailTypes");
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 8, 13, 47, 57, 991, DateTimeKind.Local).AddTicks(1447));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 8, 13, 47, 57, 995, DateTimeKind.Local).AddTicks(8902));
+        }
+    }
+}
