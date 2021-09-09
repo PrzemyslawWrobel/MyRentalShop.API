9e83a578f1a64e6e8db3bef94d9b26ae1a146a6c
PW <przemek.wrobel@poczta.fm>
2021-09-09 15:16:14 +0200

    Dodana konfiguracja ItemConfiguration dla Item

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Configurations/ItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRentalShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance.Configurations
+{
+    public class ItemConfiguration : IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100);
+            builder.Property(p => p.SecondName).HasMaxLength(200);
+            builder.Property(p => p.InventoryNumber).HasMaxLength(100);
+            builder.Property(p => p.MinAgeBorrower).HasMaxLength(100);
+        }
+    }
+}
