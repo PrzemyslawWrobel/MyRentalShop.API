7cc50d57d7a91b2fdc0e67c2c1a36f67d5605c76
PW <przemek.wrobel@poczta.fm>
2021-09-09 16:05:26 +0200

    Dodana konfiguracja ContactDetailConfiguration dla ContactDetail

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Configurations/ContactDetailConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRentalShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance.Configurations
+{
+    public class ContactDetailConfiguration : IEntityTypeConfiguration<ContactDetail>
+    {
+        public void Configure(EntityTypeBuilder<ContactDetail> builder)
+        {
+            builder.Property(p => p.ContactDetailInformation).HasMaxLength(100);
+        }
+    }
+}
