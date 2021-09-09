6648cdbc18510b91123b436f102f57d412bb5cf1
PW <przemek.wrobel@poczta.fm>
2021-09-09 16:07:22 +0200

    Zrobiona migracja Configurations

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Migrations/20210909140611_Configurations.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRentalShop.Persistance.Migrations
+{
+    public partial class Configurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemTypes",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemTags",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Items",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CustomerTypes",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactDetailTypes",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactDetailInformation",
+                table: "ContactDetails",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 16, 6, 10, 163, DateTimeKind.Local).AddTicks(8568));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 16, 6, 10, 170, DateTimeKind.Local).AddTicks(8403));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemTags",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondName",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CustomerTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactDetailTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactDetailInformation",
+                table: "ContactDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 13, 52, 42, 412, DateTimeKind.Local).AddTicks(5654));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RegistrationDate",
+                value: new DateTime(2021, 9, 9, 13, 52, 42, 417, DateTimeKind.Local).AddTicks(759));
+        }
+    }
+}
