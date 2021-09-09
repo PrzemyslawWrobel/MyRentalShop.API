fb656d4910dd32bbb5c5efd76df1439757aa7acd
PW <przemek.wrobel@poczta.fm>
2021-09-09 15:31:49 +0200

    Dodana konfiguracja     public class CustomerTypeConfiguration : IEntityTypeConfiguration<CustomerType> dla Item

---
+++ MyRentalShop.API/MyRentalShop.Persistance/Configurations/CustomerTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRentalShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Persistance.Configurations
+{
+    public class CustomerTypeConfiguration : IEntityTypeConfiguration<CustomerType>
+    {
+        public void Configure(EntityTypeBuilder<CustomerType> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100);
+        }
+    }
+}
