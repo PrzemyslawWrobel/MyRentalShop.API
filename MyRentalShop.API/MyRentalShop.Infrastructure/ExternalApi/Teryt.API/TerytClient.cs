d590812d7a2ea1d7f8eb12af47f56f7f6e85ad4f
PW <przemek.wrobel@poczta.fm>
2021-09-10 10:24:52 +0200

    Implementacja Interfejsu IterytClient

--- MyRentalShop.API/MyRentalShop.Infrastructure/ExternalApi/Teryt.API/TerytClient.cs
+++ MyRentalShop.API/MyRentalShop.Infrastructure/ExternalApi/Teryt.API/TerytClient.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using MyRentalShop.Application.Common.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyRentalShop.Infrastructure.ExternalApi.Teryt.API
 {
-    public class TerytClient
+    public class TerytClient : ITerytClient
     {
+        public Task<string> GetAdress(string searchFilter, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
