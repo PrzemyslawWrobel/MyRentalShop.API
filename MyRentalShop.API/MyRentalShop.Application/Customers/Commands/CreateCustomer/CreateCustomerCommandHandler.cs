8f820f1ecb7de3c2b6fb7fc5deefa4a716b178ae
PW <przemek.wrobel@poczta.fm>
2021-09-13 15:02:41 +0200

    ...

---
+++ MyRentalShop.API/MyRentalShop.Application/Customers/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MyRentalShop.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Application.Customers.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, int>
+    {
+        private readonly IMyRentalShopDbContext _context;
+        public CreateCustomerCommandHandler(IMyRentalShopDbContext myRentalShopDbContext)
+        {
+            _context = myRentalShopDbContext;
+        }
+
+        public Task<int> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
