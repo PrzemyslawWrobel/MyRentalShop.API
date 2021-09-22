80f8001df402d196fd4beac296d604035882bf8a
PW <przemek.wrobel@poczta.fm>
2021-09-22 13:54:41 +0200

    DeleteCustomerCommand

---
+++ MyRentalShop.API/MyRentalShop.Application/Customers/Commands/DeleteCustomerCommand/DeleteCustomerCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyRentalShop.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Application.Customers.Commands.DeleteCustomerCommand
+{
+    public class DeleteCustomerCommandHandler : IRequestHandler<DeleteCustomerCommand>
+    {
+        private readonly IMyRentalShopDbContext _context;
+
+        public DeleteCustomerCommandHandler(IMyRentalShopDbContext myRentalShopDbContext)
+        {
+            _context = myRentalShopDbContext;
+        }
+     
+        public async Task<Unit> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customer = await _context.Customers.Where(p => p.Id == request.CustomerId).FirstOrDefaultAsync(cancellationToken);
+            _context.Customers.Remove(customer);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+
+        }
+    }
+}
