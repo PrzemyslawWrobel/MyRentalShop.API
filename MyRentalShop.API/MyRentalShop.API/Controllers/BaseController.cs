24dbc2e28b953c8db3a63b5157d5cc7667a1f077
PW <przemek.wrobel@poczta.fm>
2021-09-24 14:13:30 +0200

    Użycie MiediaR w kontrolerach

--- MyRentalShop.API/MyRentalShop.API/Controllers/BaseController.cs
+++ MyRentalShop.API/MyRentalShop.API/Controllers/BaseController.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MyRentalShop.API.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class BaseController : ControllerBase
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
--- MyRentalShop.API/MyRentalShop.API/Controllers/CustomersController.cs
+++ MyRentalShop.API/MyRentalShop.API/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.UI.V4.Pages.Internal;
 using Microsoft.AspNetCore.Mvc;
+using MyRentalShop.Application.Customers.Queries.GetCustomerDetail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,10 @@ namespace MyRentalShop.API.Controllers
     /// <summary>
     /// Kontroler odp za obsługę użytkowników
     /// </summary>
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/customers")]
     [EnableCors("MyAllowAngularOrigins")]
 
-    public class CustomerController : ControllerBase
+    public class CustomersController : BaseController
     {
         /// <summary>
         /// Zwraca listę użytkowników
@@ -29,6 +29,19 @@ namespace MyRentalShop.API.Controllers
         public async Task<ActionResult<string>> GetCustomersAsync()
         {
             return "Customer Controller";
+        }
+
+        /// <summary>
+        /// Zwraca użytkownika po id
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<GetCustomerDetailVm>> GetCustomerDetails(int id)
+        {
+            var vm = await Mediator.Send(new GetCustomerDetailQuery() { CustomerId = id });
+            return vm;
         }
     }
 }
