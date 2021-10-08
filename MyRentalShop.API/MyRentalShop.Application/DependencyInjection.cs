3002db5a9fded5bce7b9b5b5505371975cec15e1
PW <przemek.wrobel@poczta.fm>
2021-10-08 11:11:27 +0200

    Dodany do dependencyInjection PerformanceBehaviour

--- MyRentalShop.API/MyRentalShop.Application/DependencyInjection.cs
+++ MyRentalShop.API/MyRentalShop.Application/DependencyInjection.cs
@@ -19,6 +19,7 @@ namespace MyRentalShop.Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             services.AddTransient(typeof(IRequestPreProcessor<>), typeof(LoggingBehaviour<>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
             return services;
         }
     }
