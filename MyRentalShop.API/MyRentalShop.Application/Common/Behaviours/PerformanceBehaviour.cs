ac7dc12a3a9bfdc5ef2729436aa87902c5038582
PW <przemek.wrobel@poczta.fm>
2021-10-08 11:14:51 +0200

    Dodany PerformanceBehaviour cd

--- MyRentalShop.API/MyRentalShop.Application/Common/Behaviours/PerformanceBehaviour.cs
+++ MyRentalShop.API/MyRentalShop.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyRentalShop.Application.Common.Behaviours
 {
-    class PerformanceBehaviour
+    public class PerformanceBehaviour<TRequest, TResponce> : IPipelineBehavior<TRequest, TResponce>
     {
+        private readonly ILogger _logger;
+        private readonly Stopwatch _timer;
+        public PerformanceBehaviour(ILogger<TRequest> logger)
+        {
+            _timer = new Stopwatch();
+            _logger = logger;
+        }
+        public async Task<TResponce> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponce> next)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsed = _timer.ElapsedMilliseconds;
+
+            if(elapsed > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+
+                _logger.LogInformation("MyRentalShop Request: {Name} ({elapsed} milliseconds) {@Request}", requestName, elapsed, request);
+            }
+
+            return response;
+        }
     }
 }
