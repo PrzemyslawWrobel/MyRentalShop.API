5838df773126fa2740af9af2dcd67db796f9bc25
PW <przemek.wrobel@poczta.fm>
2021-10-14 11:05:07 +0200

    Test for Configuration Automapper

--- MyRentalShop.API/Application.UnitTests/Mapping/MappingTest.cs
+++ MyRentalShop.API/Application.UnitTests/Mapping/MappingTest.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Application.UnitTests.Mapping
 {
-    public class MappingTest
+    public class MappingTest : IClassFixture<MappingTestFixture>
     {
+        private readonly IConfigurationProvider _configurationProvider;
+        private readonly IMapper _mapper;
+
+        public MappingTest(MappingTestFixture mappingTestFixture)
+        {
+            _configurationProvider = mappingTestFixture.ConfigurationProvider;
+            _mapper = mappingTestFixture.Mapper;
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configurationProvider.AssertConfigurationIsValid();
+        }
     }
 }
